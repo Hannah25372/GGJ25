96f96fc0baeb6f804e86c8949d665a149f9c1868
Hannah25372 <127040735+Hannah25372@users.noreply.github.com>
2025-01-25 14:01:01 +0000

    hit

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -6,37 +6,69 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     Rigidbody2D body;
-    public Vector2 move;
+
     public float horizontal;
     public float vertical;
+    public float aimHorizontal;
+    public float aimVertical;
+    public bool fired;
 
     float moveLimiter = 0.7f;
     public float speed = 2f;
+
+    public GameObject bullet;
+    private Rigidbody2D bulletBody;
+    public float bulletSpeed = 4f;
+
+    public GameObject enemyBullet;
+
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        bulletBody = bullet.GetComponent<Rigidbody2D>();
+        bullet.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Debug.Log("HIT");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //movement
         if (horizontal != 0 && vertical != 0) // Check for diagonal movement
         {
-            // limit movement speed diagonally, so you move at 70% speed
             horizontal *= moveLimiter;
             vertical *= moveLimiter;
-
         }
         body.velocity = new Vector2(horizontal * speed, vertical * speed);
+
     }
 
     private void OnMove(InputValue value)
     {
         horizontal = value.Get<Vector2>().x;
         vertical = value.Get<Vector2>().y;
+    }
 
+    private void OnLook(InputValue value)
+    {
+        aimHorizontal = value.Get<Vector2>().x;
+        aimVertical = value.Get<Vector2>().y;
+    }
+
+    private void OnFire(InputValue value)
+    {
+        //shoot in direction of aim.
+        bullet.transform.position = new Vector2(transform.position.x + aimHorizontal, transform.position.y + aimVertical);
+        bullet.SetActive(true);
+        bulletBody.velocity = (new Vector2(aimHorizontal * bulletSpeed, aimVertical * bulletSpeed));
     }
 
 
