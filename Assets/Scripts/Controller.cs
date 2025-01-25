d887f15054b97893d2fb9a52b3188d6b8cf9f464
Hannah25372 <127040735+Hannah25372@users.noreply.github.com>
2025-01-25 16:18:58 +0000

    health and death

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
+using TMPro;
 
 public class Controller : MonoBehaviour
 {
@@ -9,19 +10,25 @@ public class Controller : MonoBehaviour
 
     public float horizontal;
     public float vertical;
-    public float aimHorizontal;
-    public float aimVertical;
-    public bool fired;
-
     float moveLimiter = 0.7f;
     public float speed = 2f;
+    public bool dead = false;
+
+    public int health = 5;
+    public int attackDamage = 1;
 
     public GameObject bullet;
     private Rigidbody2D bulletBody;
+    public float aimHorizontal;
+    public float aimVertical;
     public float bulletSpeed = 4f;
 
-    public GameObject enemyBullet;
+    public Controller opponent;
 
+    public TextMeshProUGUI healthText;
+    
+    enum Phase { One = 1, Two = 2}
+    Phase phase;
 
     // Start is called before the first frame update
     void Start()
@@ -29,14 +36,41 @@ public class Controller : MonoBehaviour
         body = GetComponent<Rigidbody2D>();
         bulletBody = bullet.GetComponent<Rigidbody2D>();
         bullet.SetActive(false);
+        phase = Phase.One;
+        healthText.text = "Health: " + health.ToString();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Bullet"))
+        //Current player collided with opponent bullet
+        if (collision.gameObject.CompareTag("Bullet") && collision.gameObject!=bullet)
         {
-            Debug.Log("HIT");
+            TakeDamage();
+        } 
+        else if (collision.gameObject.CompareTag("BubbleVial"))
+        {
+
+
         }
+        else if (collision.gameObject.CompareTag("BubbleMachine") && phase==Phase.One)
+        {
+
+
+        }
+
+    }
+
+    private void TakeDamage()
+    {
+        health -= opponent.attackDamage;
+        opponent.bullet.SetActive(false);
+        if (health <= 0)
+        {
+            health = 0;
+            dead = true;
+        }
+        //update UI
+        healthText.text = "Health: " + health.ToString();
     }
 
     void FixedUpdate()
@@ -49,6 +83,10 @@ public class Controller : MonoBehaviour
         }
         body.velocity = new Vector2(horizontal * speed, vertical * speed);
 
+    }
+
+    void Update()
+    {
     }
 
     private void OnMove(InputValue value)
@@ -65,10 +103,18 @@ public class Controller : MonoBehaviour
 
     private void OnFire(InputValue value)
     {
-        //shoot in direction of aim.
-        bullet.transform.position = new Vector2(transform.position.x + aimHorizontal, transform.position.y + aimVertical);
-        bullet.SetActive(true);
-        bulletBody.velocity = (new Vector2(aimHorizontal * bulletSpeed, aimVertical * bulletSpeed));
+        if (phase == Phase.One)
+        {
+            //place item
+
+        }
+        if (phase == Phase.Two)
+        {
+            //shoot in direction of aim.
+            bullet.transform.position = new Vector2(transform.position.x + aimHorizontal, transform.position.y + aimVertical);
+            bullet.SetActive(true);
+            bulletBody.velocity = (new Vector2(aimHorizontal * bulletSpeed, aimVertical * bulletSpeed));
+        }     
     }
 
 
